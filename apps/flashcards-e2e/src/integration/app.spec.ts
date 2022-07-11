fa506b134bbd46168c147328cdf76deb3d595bdb
Aurelien Rodot <aurelien.rodot@gmail.com>
2022-07-11 16:39:57 +0200

    fix: flashcards e2e test

--- apps/flashcards-e2e/src/integration/app.spec.ts
+++ apps/flashcards-e2e/src/integration/app.spec.ts
@@ -8,6 +8,6 @@ describe('flashcards', () => {
     cy.login('my-email@something.com', 'myPassword');
 
     // Function helper example, see `../support/app.po.ts` file
-    getGreeting().contains('Welcome flashcards');
+    getGreeting().contains('Welcome to flashcards');
   });
 });
